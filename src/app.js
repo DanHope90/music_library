a11cf15cb0ea02bb39fb07ed7b8442bc296118aa
Dan Hope <dplhope@gmail.com>
2022-02-19 20:50:42 +0000

    first test complete

--- src/app.js
+++ src/app.js
@@ -2,7 +2,11 @@ const express = require('express');
 
 const app = express();
 
+const router = require('./routes/artist');
+
 app.use(express.json());
+
+app.use('/artist', router);
 
 app.get('/', (req, res) => {
     res.status(200).send('Hello World' );
